1ea6f2783eb9d1f784c93f983ded15331e6c1ed8
Blake Ribble <Bribble3982@conestogac.on.ca>
2018-11-26 16:45:04 -0500

    Add project files.

---
+++ TDDAssignment/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDAssignment
+{
+    public class Triangle
+    {
+        public static double GetHypotenuse(string Length1, string Length2)
+        {
+            return 0;
+        }
+    }
+}
