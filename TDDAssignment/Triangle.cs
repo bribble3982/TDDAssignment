1dd8cb78d9a70c3eb4ce215cca49875c537d6ac6
Blake Ribble <Bribble3982@conestogac.on.ca>
2018-11-26 17:11:01 -0500

    Test 1 Area failed - non numeric strings entered, therefore test failed

--- TDDAssignment/Triangle.cs
+++ TDDAssignment/Triangle.cs
@@ -12,15 +12,15 @@ namespace TDDAssignment
         {
             try
             {
-                double sideLength1 = Convert.ToDouble(Length1);
-                double sideLength2 = Convert.ToDouble(Length2);
+                double sideA = Convert.ToDouble(Length1);
+                double sideB = Convert.ToDouble(Length2);
 
-                if(sideLength1 <= 0 || sideLength2 <= 0)
+                if(sideA <= 0 || sideB <= 0)
                 {
                     return -2;
                 }
 
-                double hypotenuse = Math.Sqrt((sideLength1 * sideLength1) + (sideLength2 * sideLength2));
+                double hypotenuse = Math.Sqrt((sideA * sideA) + (sideB * sideB));
 
                 return hypotenuse; 
             }
@@ -34,7 +34,12 @@ namespace TDDAssignment
 
         public static double GetArea(string Length1, string Length2)
         {
-            return 0;
+            double sideA = Convert.ToDouble(Length1);
+            double sideB = Convert.ToDouble(Length2);
+
+            double area = sideA * sideB * 0.5;
+
+            return area;
         }
     }
 }
